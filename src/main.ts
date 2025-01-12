cf19a9590ea1e73a12d3679a57c315e2c6d384c5
abhikatta <abhinaykatta97@gmail.com>
2025-01-13 00:50:47 +0530

    some changes

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,9 @@ import { GetHighestScore } from "./lib/HighScore";
 
 const app = document.getElementById("app")!;
 const canvas = document.createElement("canvas");
-const message = document.createElement("p");
 const scoreElement = document.createElement("p");
 const highScoreElement = document.createElement("h2");
-message.className = "errorMessage";
 
-let direction: Direction = { x: 0, y: 0 };
-
-app.append(message);
 app.append(canvas);
 app.append(scoreElement);
 app.append(highScoreElement);
@@ -25,13 +20,13 @@ app.append(highScoreElement);
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
+let direction: Direction = { x: 0, y: 0 };
 const snake = new Snake(canvas);
 const score = new Score();
 const foodItem = new Food(canvas, snake, score);
+
 window.addEventListener("keydown", (event: KeyboardEvent) => {
   const [newDirection] = ChangeDirection(direction, event);
-
-  // TODO:  message.innerText = errorMessage;
   direction = newDirection;
 });
 
