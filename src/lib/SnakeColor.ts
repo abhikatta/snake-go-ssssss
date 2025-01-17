464e201e8542635322669e35d5635357195797cb
abhikatta <abhinaykatta97@gmail.com>
2025-01-17 14:07:50 +0530

    snake color remove check

--- src/lib/SnakeColor.ts
+++ src/lib/SnakeColor.ts
@@ -1,17 +1,8 @@
 import { snakeColorType } from "../types";
 
 export const selectColor = (e: Event) => {
-  const snakeColor = getSnakeColor();
-  if (Boolean(snakeColor)) {
-    return alert(
-      `Color already selected as ${snakeColor
-        ?.charAt(0)
-        .toLocaleUpperCase()}${snakeColor?.slice(1)}!`
-    );
-  } else {
-    const color = (e.target as HTMLSelectElement).value;
-    localStorage.setItem("snakeColor", color);
-  }
+  const color = (e.target as HTMLSelectElement).value;
+  localStorage.setItem("snakeColor", color);
 };
 
 export const getSnakeColor = (): snakeColorType | "gray" => {
